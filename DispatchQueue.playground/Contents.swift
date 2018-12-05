63fb9988e42ce755628af5d12a23a96e7037b0fb
Shawon Ashraf <shawon13@live.com>
2018-12-06 03:35:01 +0600

    asyncAfter and serial queue

--- DispatchQueue.playground/Contents.swift
+++ DispatchQueue.playground/Contents.swift
@@ -20,13 +20,30 @@ let concurrentQueue = DispatchQueue(label: "cqueue.shawon", attributes: .concurr
 let serialQueue = DispatchQueue(label: "squeue.shawon")
 
 concurrentQueue.async(execute: {
-    performCalc(iterations: 10000, tag: "async1")
+    performCalc(iterations: 10000, tag: "concurrent async1")
 })
 
 concurrentQueue.async(execute: {
-    performCalc(iterations: 100, tag: "async2")
+    performCalc(iterations: 100, tag: "concurrent async2")
 })
 
 concurrentQueue.async(execute: {
-    performCalc(iterations: 1000, tag: "async3")
+    performCalc(iterations: 1000, tag: "concurrent async3")
 })
+
+// check for serial queue
+
+serialQueue.async(execute: { performCalc(iterations: 10000, tag: "serial async1") })
+serialQueue.async(execute: { performCalc(iterations: 1000, tag: "serial async2") })
+serialQueue.async(execute: { performCalc(iterations: 100, tag: "serial async3") })
+
+// asyncAfter
+
+let waitingQueue = DispatchQueue(label: "wqueue.shawon")
+
+let delayInSeconds = 2.0
+let pTime = DispatchTime.now() + Double(Int64(delayInSeconds * Double(NSEC_PER_SEC))) / Double(NSEC_PER_SEC)
+
+waitingQueue.asyncAfter(deadline: pTime) {
+    print("Time's up!")
+}
