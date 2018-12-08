6ea82ee92c2fd5ad9817cb7b3c8f4db1525df973
Shawon Ashraf <shawon13@live.com>
2018-12-08 22:31:45 +0600

    rest get json parse

--- JSON.playground/Contents.swift
+++ JSON.playground/Contents.swift
@@ -2,40 +2,42 @@ import Cocoa
 
 // JSON Structure
 struct Game: Codable {
-    var name: String
-    var platform: String
-    var publisher: String
+    let  _id: String
+    let name: String
+    let platform: String
+    let publisher: String?
 }
 
 struct Games: Codable {
-    var games: [Games]
+    let games: [Game]
+}
+
+let jsonString = """
+{"games":[{"_id":"5b8e4e77ece19b04ae342842","name":"Forza Horizon 3","platform":"XBOXONE","publisher":"Microsoft Studios"},{"_id":"5bc0c4effb6fc060274322d3","name":"Forza Horizon 4","publisher":"Microsoft Studios","platform":"XBOXONE"},{"_id":"5b8e4e76ece19b04ae342822","name":"Halo Wars 2","platform":"XBOXONE","publisher":"Microsoft Studios"},{"_id":"5b8e4e76ece19b04ae342824","name":"Rise of The Tomb Raider","platform":"XBOXONE","publisher":"Square Enix"},{"_id":"5bfee02be7179a10a042afdc","name":"Super Lucky's Tale","platform":"XBOXONE","publisher":"Microsoft Studios"}]}
+"""
+// decode
+let data = jsonString.data(using: .utf8)
+if let jsonData = data {
+    do {
+        let response = try JSONDecoder().decode(Games.self, from: jsonData)
+        for game in response.games {
+            print(game)
+        }
+    } catch let error {
+        print(error)
+    }
 }
 
 let encoder = JSONEncoder()
 let decoder = JSONDecoder()
 
-let rdr2 = Game(name: "Red Dead Redemption 2", platform: "PS4", publisher: "Rockstar")
+let rdr2 = Game(_id: "909", name: "Red Dead Redemption 2", platform: "PS4", publisher: "Rockstar")
 let rdr2JSON = try? encoder.encode(rdr2)
 
-//if let data = rdr2JSON, let dataString = String(data: data, encoding: .utf8) {
-//    print(dataString)
-//}
-
-func encodeToJSON(from data: Game) -> Data? {
-    let encoder = JSONEncoder()
-    encoder.outputFormatting = .prettyPrinted
-    return try? encoder.encode(data)
+if let data = rdr2JSON, let dataString = String(data: data, encoding: .utf8) {
+    print(dataString)
 }
 
-func decodeJSON(json: Data) -> String? {
-    let dataString = String(data: json, encoding: .utf8)
-    return dataString
-}
-
-//if let json = encodeToJSON(from: rdr2) {
-//    let str = decodeJSON(json: json) ?? ""
-//    print(str)
-//}
 
 // check from API request
 class GetClient {
@@ -49,8 +51,16 @@ class GetClient {
     typealias postCallBackClosure = (URLResponse?, Data?) -> Void
     let printResponse: postCallBackClosure = {
         if let data = $1 {
-            let dataString = String(data: data, encoding: String.Encoding.init(rawValue: String.Encoding.utf8.rawValue)) ?? ""
-            print(dataString)
+            // decode as object
+            do {
+                let response = try JSONDecoder().decode(Games.self, from: data)
+                print("Got \(data) from API with \(response.games.count) games")
+                for game in response.games {
+                    print(game)
+                }
+            } catch let error {
+                print(error)
+            }
         }
     }
     
@@ -89,4 +99,3 @@ class GetClient {
 
 let client = GetClient(apiURL: "localhost:3000", apiEndpoint: "/games/PS4")
 let responseData = client.get(handler: client.printResponse)
-
